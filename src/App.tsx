8da187b826020cbddaabfbe983e39cae9b154c32
krzysiek <krzysiek@Krzysztofs-MacBook-Pro-2.local>
2023-05-01 14:32:35 +0200

    move config file , remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
 import './App.css';
-import { Button, Link, Container, Input } from './components/ui/atoms';
+import { Container } from './components/ui/atoms';
 
 function App() {
   return (
     <Container>
-      <Button>Button</Button>
-      <Link type="text">go to </Link>
-      <Input type='email'/>
     </Container>
   );
 }
